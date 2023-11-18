c9b414e6ac6d84a31b496ac002fa90a3c2b931ba
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-11-18 15:37:39 -0300

    first commit

---
+++ TestingColorPicker/TestingColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingColorPickerApp.swift
+//  TestingColorPicker
+//
+//  Created by thaxz on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
