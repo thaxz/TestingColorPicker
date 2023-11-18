609050d524971ff52b20965885c245078e8aa12d
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-11-18 16:59:10 -0300

    feature(ColorPicker): Finishing custom grid view

--- TestingColorPicker/GridColorPicker.swift
+++ TestingColorPicker/GridColorPicker.swift
@@ -14,12 +14,38 @@ struct GridColorPicker: View {
     
     let colorOptions = CustomColor.spectrum()
     var body: some View {
-        ScrollView([.horizontal, .vertical]){
-            LazyVGrid(columns: Array(repeating: GridItem(.fixed(colorSize), spacing: 0), count: 21), spacing: 0) {
-                ForEach(colorOptions) { color in
-                    Color(hue: color.hue, saturation: color.saturation, brightness: color.brightness)
+        VStack {
+            ScrollView([.horizontal, .vertical]){
+                LazyVGrid(columns: Array(repeating: GridItem(.fixed(colorSize), spacing: 0), count: 21), spacing: 0) {
+                    ForEach(colorOptions) { color in
+                        ZStack {
+                            if color == selectedColor {
+                                Rectangle()
+                                    .fill(Color.white)
+                                    .padding(-5)
+                            }
+                            Color(hue: color.hue, saturation: color.saturation, brightness: color.brightness)
+                        }
                         .frame(height: colorSize)
+                        .id(color)
+                        .zIndex(color == selectedColor ? 100 : 1)
+                    }
                 }
+                .padding(350)
+                // .scrollTargetLayout()
+            }
+            // .scrollPosition(id: $selectedColor, anchor: .center)
+            
+            HStack{
+                Text("Color selected:")
+                if let selectedColor{
+                    Color(hue: selectedColor.hue, saturation: selectedColor.saturation, brightness: selectedColor.brightness)
+                        .frame(height: 20)
+                    
+                }
+            }
+            .onAppear{
+                selectedColor = colorOptions[colorOptions.count / 2]
             }
         }
     }
