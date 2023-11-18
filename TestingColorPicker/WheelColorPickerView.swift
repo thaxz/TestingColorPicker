8e44218261ccc140f789a1fa8c22bf089fc19756
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-11-18 16:09:00 -0300

    feature(ColorPicker): Using visual effects and geometry proxy to calculate opacity

--- TestingColorPicker/WheelColorPickerView.swift
+++ TestingColorPicker/WheelColorPickerView.swift
@@ -26,6 +26,10 @@ struct WheelColorPickerView: View {
                             }
                         }
                         .zIndex(index == selectedColorIndex ? 100 : 1)
+                        .visualEffect { content, proxy in
+                            content
+                                .opacity(opacity(for: proxy))
+                        }
                 }
                 Color.white
                     .aspectRatio(2, contentMode: .fit)
@@ -45,6 +49,20 @@ struct WheelColorPickerView: View {
         }
         .frame(height: 100)
     }
+}
+
+// MARK: Functions
+
+extension WheelColorPickerView {
+    
+    private func opacity(for proxy: GeometryProxy) -> Double {
+        let scrollViewHeight = proxy.bounds(of: .scrollView)?.height ?? 100
+        let rowCenterPosition = proxy.frame(in: .scrollView).midY
+        let distanceFromScrollCenter = abs(scrollViewHeight / 2 - rowCenterPosition)
+        let opacityValue = 1 - Double(distanceFromScrollCenter / scrollViewHeight) * 2
+        return opacityValue
+    }
+    
 }
 
 struct WheelColorPickerView_Previews: PreviewProvider {
